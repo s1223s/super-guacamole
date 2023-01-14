394efc81998e6cba14d293bcdca9bd8baf675de6
s1223s <qinjiajun03@gmail.com>
2023-01-14 16:01:18 +0800

    update .gitignore

---
+++ bigdata/src/test/java/com/archillec/bigdata/BigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archillec.bigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
